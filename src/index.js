dcc052e775c11170e1a4a77a78055b9d9ce2acd1
Larkin Williams-Capone <larkinwc@gmail.com>
2018-02-18 21:49:14 -0600

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// change require to es6 import style
-import $ from 'jquery';
-import './style.scss';
-
-let x = 0;
-setInterval(() => { x += 1; $('#main').html('You have been here for ' + x + ' seconds'); }, 1000);
